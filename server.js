8519669e6eb1ec9a26b7af6fe0cd8eca84cadec5
Gustavo Pedralino <gpedralino@gmail.com>
2019-01-29 12:26:49 -0200

    general changes

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 8080;
+
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/AngularRPG'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/AngularRPG/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-//app.listen(process.env.PORT || 8080);
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-});
+app.listen(process.env.PORT || 8080);
